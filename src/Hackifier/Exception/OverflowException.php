ba3493506ac94687bf51f5902e24a4c196a98825
azjezz <azjezz@protonmail.com>
2019-03-31 12:02:47 +0100

    add overflow exception

---
+++ src/Hackifier/Exception/OverflowException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Hackifier package.
+ *
+ * (c) Saif Eddin Gmati <azjezz@protonmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hackifier\Exception;
+
+use OverflowException as ParentException;
+
+class OverflowException extends ParentException implements IException
+{
+}
