3ba11cac735fedeb708f93c767addba1dd624312
azjezz <azjezz@protonmail.com>
2019-03-31 12:01:43 +0100

    add magic const transformer

---
+++ src/Hackifier/Transformer/Scalar/MagicConstantTransformer.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Hackifier package.
+ *
+ * (c) Saif Eddin Gmati <azjezz@protonmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hackifier\Transformer\Scalar;
+
+use Hackifier\HackAST\EditableNode;
+use function Hackifier\HackAST\Missing;
+use Hackifier\HackAST\Token\NameToken;
+use Hackifier\ITransformer;
+use Hackifier\Transformer\AbstractTransformer;
+use PhpParser\Node;
+
+/**
+ * @extends AbstractTransformer<Node\Scalar\MagicConst>
+ */
+class MagicConstantTransformer extends AbstractTransformer
+{
+    /**
+     * @param Node\Scalar\MagicConst $node
+     * @param ITransformer           $transformer
+     *
+     * @return EditableNode
+     */
+    public function transform($node, ITransformer $transformer): EditableNode
+    {
+        return $this->comments($node, new NameToken(Missing(), Missing(), $node->getName()));
+    }
+
+    public function supports(Node $node): bool
+    {
+        return $node instanceof Node\Scalar\MagicConst;
+    }
+}
