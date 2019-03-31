e666769258cd1d74b9fb89c8d873d023d2743867
azjezz <azjezz@protonmail.com>
2019-03-31 03:24:56 +0100

    add statement expression transformer

---
+++ src/Hackifier/Transformer/Statement/ExpressionTransformer.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Hackifier package.
+ *
+ * (c) Saif Eddin Gmati <azjezz@protonmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Hackifier\Transformer\Statement;
+
+use Hackifier\HackAST\EditableNode;
+use function Hackifier\HackAST\Missing;
+use Hackifier\HackAST\Token\SemicolonToken;
+use Hackifier\HackAST\Trivia\EndOfLine;
+use Hackifier\ITransformer;
+use Hackifier\Transformer\AbstractTransformer;
+use PhpParser\Node;
+
+/**
+ * @extends AbstractTransformer<Node\Stmt\Expression>
+ */
+class ExpressionTransformer extends AbstractTransformer
+{
+    /**
+     * @param Node\Stmt\Expression $node
+     * @param ITransformer $transformer
+     *
+     * @return EditableNode
+     */
+    public function transform($node, ITransformer $transformer): EditableNode
+    {
+        return $this->comments($node, $this->list(
+            $transformer->transform($node->expr),
+            new SemicolonToken(Missing(), new EndOfLine("\n"))
+        ));
+    }
+
+    public function supports(Node $node): bool
+    {
+        return $node instanceof Node\Stmt\Expression;
+    }
+}
